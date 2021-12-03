9344cf9543212b5010d053d6a4b19bfa4505b620
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-12-03 15:02:04 -0800

    created flashcard component

--- amgen/src/App.js
+++ amgen/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+//import my components
+import FlashCard from './components/FlashCard/FlashCard';
 function App() {
   return (
     <div className="App">
         Amgen Associate Software Engineer Interview Homework
+        <FlashCard />
     </div>
   );
 }
--- amgen/src/components/FlashCard/FlashCard.js
+++ amgen/src/components/FlashCard/FlashCard.js
@@ -1,1 +1,22 @@
 import React from 'react';
+//import material ui components
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+const FlashCard = () => {
+    return (
+        <Card sx={{minWidth: 275}}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14}} color="text.secondary">
+                    Quote
+                </Typography>
+                <Typography variant="h1">
+                    "Quote goes here."
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default FlashCard;
