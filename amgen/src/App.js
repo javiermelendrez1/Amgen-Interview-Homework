b6695dac21cf868e699ab6d80a8cd4dbfcc9d735
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-12-03 14:22:17 -0800

    deleted unused files

---
+++ amgen/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Amgen Associate Software Engineer Interview Homework
+    </div>
+  );
+}
+
+export default App;
